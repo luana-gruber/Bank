686ba6d6466f37baa2728f64443d7fb2424d4549
joao <joaotnf@gmail.com>
2022-08-04 09:13:46 -0300

    projeto spring inicializado

---
+++ g3bank/src/main/java/com/accenture/academico/g3bank/G3bankApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.academico.g3bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G3bankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G3bankApplication.class, args);
+	}
+
+}
