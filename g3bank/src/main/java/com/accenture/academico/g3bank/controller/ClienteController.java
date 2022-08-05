19d8083188d6b3ae23dda6ecdb18a0da2ed4371e
luana-gruber <luana_gruber@hotmail.com>
2022-08-05 16:57:55 -0300

    Modificação application.proprities

--- g3bank/src/main/java/com/accenture/academico/g3bank/controller/ClienteController.java
+++ g3bank/src/main/java/com/accenture/academico/g3bank/controller/ClienteController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,7 @@ import com.accenture.academico.g3bank.repository.ClienteRepository;
 
 import io.swagger.annotations.ApiOperation;
 
-
-
+@Controller
 @RestController
 @RequestMapping(path = "/g3bank")
 public class ClienteController {
